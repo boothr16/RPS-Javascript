e2a3e6f46a7b19d0c9d1987ac1a3406d697baad5
Robert Booth <boothr16@gmail.com>
2021-05-16 19:06:30 -0400

    Tested getUserChoice() function

--- RPS.js
+++ RPS.js
@@ -1,11 +1,13 @@
+// randomly selects rock, paper, or scissors
 function computerPlay() {
     const choices = ["rock", "paper", "scissors"];
     const min = 0;
     const max = choices.length - 1;
-    const rand = Math.floor(Math.random() * (max - min + 1)) + min;
-    console.log(choices[rand]);
+    const rand = Math.floor(Math.random() * (max - min + 1)) + min; // true random, inclusive range
+    return choices[rand];
 }
-
-function singleRound(playerSelection, computerSelection) {
-    
+// gets user input
+function getUserChoice() {
+    let input = prompt("Choose rock, paper, or scissors.").toLowerCase();
+    return input;
 }
