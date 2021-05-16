668f5edae1404724e7d87cd7a24ad8fc7de2111d
Robert Booth <boothr16@gmail.com>
2021-05-16 19:20:50 -0400

    Tested playRound() function.

--- RPS.js
+++ RPS.js
@@ -4,6 +4,7 @@ function computerPlay() {
     const min = 0;
     const max = choices.length - 1;
     const rand = Math.floor(Math.random() * (max - min + 1)) + min; // true random, inclusive range
+    console.log(choices[rand]);
     return choices[rand];
 }
 // gets user input
@@ -11,3 +12,38 @@ function getUserChoice() {
     let input = prompt("Choose rock, paper, or scissors.").toLowerCase();
     return input;
 }
+// compares result of computerPlay() and getUserChoice() functions to declare winner
+function playRound(userChoice, computerChoice) {
+    if (userChoice == computerChoice)
+        alert("Tie!");
+    else {
+        if (computerChoice == "rock") {
+            if (userChoice == "paper")
+                alert("Player wins!");
+            else if (userChoice == "scissors")
+                alert("PC wins!");
+            else
+                alert("Invalid input.");
+        }
+        else if (computerChoice == "paper") {
+            if (userChoice == "scissors")
+                alert("Player wins!");
+            else if (userChoice == "rock")
+                alert("PC wins!");
+            else
+                alert("Invalid input.");            
+        }
+        else {
+            if (userChoice == "rock")
+                alert("Player wins!");
+            else if (userChoice == "paper")
+                alert("PC wins!");
+            else
+                alert("Invalid input.");            
+        }
+    }
+}
+
+let me = getUserChoice();
+let pc = computerPlay();
+playRound(me, pc);
